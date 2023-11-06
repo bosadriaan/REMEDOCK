8bf3b19cfcb5c0a362ce357c575a85a238d57201
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-11-06 20:44:28 -0300

    trying 443

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ app.add_middleware(
 
 # client = chromadb.PersistentClient(path="/content/chroma_db")
 # client = chromadb.HttpClient(host='chroma', port=8000)
-client = chromadb.HttpClient(host='chroma', port=8000)
+# client = chromadb.HttpClient(host='chroma', port=8000)
+client = chromadb.HttpClient(host='chromadb-production.up.railway.app', port=443, scheme='https')
+
 
 
 # Embedding function setup
