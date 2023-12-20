5d949944b78d5c2958cef9c13233bd2a02fb76b7
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-12-20 12:12:53 -0300

    before using docker volume

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 
 # client = chromadb.PersistentClient(path="/content/chroma_db")
- client = chromadb.HttpClient(host='chroma', port=8000)
+client = chromadb.HttpClient(host='chroma', port=8000)
 # client = chromadb.HttpClient(host='chroma-production-6261.up.railway.app')
 
 
