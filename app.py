3563d0a49770320a783aadd1c04528e59c22af67
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-12-26 13:22:24 -0300

    something something

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 
 # client = chromadb.PersistentClient(path="/content/chroma_db")
-client = chromadb.HttpClient(host='chroma', port=8000)
+client = chromadb.HttpClient(host="chroma", port=8000)
 # client = chromadb.HttpClient(host='chroma-production-6261.up.railway.app')
 
 
@@ -49,7 +49,6 @@ sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFuncti
 )
 
 
-
 # Delete the existing collection if it exists
 
 """ collection_name = "sentences"
@@ -62,21 +61,22 @@ except ValueError as e:
 
 # Create a collection or get it if it already exists
 collection = client.get_or_create_collection(
-    name="sentences", 
+    name="sentences",
     embedding_function=sentence_transformer_ef,
-    metadata={"hnsw:space": "cosine"}
+    metadata={"hnsw:space": "cosine"},
 )
 
 
-#===============================
+# ===============================
 # Test the API is responding correctly
 @app.get("/")
 async def root():
     return {"message": "Hello, Worldski"}
 
 
-#=============================== 
+# ===============================
 # Add a document to the collection
+
 
 class Metadata(BaseModel):
     language: str
@@ -84,57 +84,58 @@ class Metadata(BaseModel):
     user_id: str
     time: str
 
+
 class DocumentIn(BaseModel):
     sentence: str
     metadata: Metadata
 
+
 class DocumentOut(DocumentIn):
     document_id: str
 
- 
+
 @app.post("/documents", response_model=DocumentOut)
 async def add_document(document: DocumentIn):
     document_id = hashlib.sha256(
-        (document.sentence + document.metadata.user_id + document.metadata.time).encode('utf-8')
+        (document.sentence + document.metadata.user_id + document.metadata.time).encode(
+            "utf-8"
+        )
     ).hexdigest()
     metadata = {
         "language": document.metadata.language,
         "country": document.metadata.country,
         "user_id": document.metadata.user_id,
-        "time": document.metadata.time
+        "time": document.metadata.time,
     }
     collection.add(
-        documents=[document.sentence],
-        ids=[document_id],
-        metadatas=[metadata]
+        documents=[document.sentence], ids=[document_id], metadatas=[metadata]
     )
     return {
         "document_id": document_id,
         "sentence": document.sentence,
-        "metadata": document.metadata
-
+        "metadata": document.metadata,
     }
-#================================
-#get document from collection with filter
+
+
+# ================================
+# get document from collection with filter
+
 
 @app.get("/documents")
-async def get_documents(
-        ids: str = None,
-        where: str = None,
-        where_document: str = None):
+async def get_documents(ids: str = None, where: str = None, where_document: str = None):
     ids_list = json.loads(ids) if ids else None
     where_dict = json.loads(where) if where else None
     where_document_dict = json.loads(where_document) if where_document else None
 
     result = collection.get(
-        ids=ids_list,
-        where=where_dict,
-        where_document=where_document_dict
+        ids=ids_list, where=where_dict, where_document=where_document_dict
     )
     return result
 
-#================================
+
+# ================================
 # delete a document from collection
+
 
 @app.delete("/documents")
 async def delete_document(document_id: str):
@@ -145,23 +146,31 @@ async def delete_document(document_id: str):
         raise HTTPException(status_code=404, detail=str(e))
 
 
-#================================
-#Query one or more IDs against filtered collection
- 
+# ================================
+# Query one or more IDs against filtered collection
+
+
 class QueryByIDData(BaseModel):
-    document_id: str = Field(..., description="The document ID to use as the basis for the query.")
+    document_id: str = Field(
+        ..., description="The document ID to use as the basis for the query."
+    )
     n_results: int = Field(4, description="The number of results to return.")
     where: Optional[dict] = Field(None, description="Metadata filter dictionary.")
-    where_document: Optional[dict] = Field(None, description="Document content filter dictionary.")
-    include: Optional[List[str]] = Field(None, description="Fields to include in the response.")
+    where_document: Optional[dict] = Field(
+        None, description="Document content filter dictionary."
+    )
+    include: Optional[List[str]] = Field(
+        None, description="Fields to include in the response."
+    )
+
 
 @app.post("/query_by_id")
 async def query_by_id(query_data: QueryByIDData):
     # Get the embedding of the document with the supplied id
     doc_data = collection.get(ids=[query_data.document_id], include=["embeddings"])
-    if not doc_data['embeddings']:
+    if not doc_data["embeddings"]:
         raise HTTPException(status_code=404, detail="Document not found")
-    query_embedding = doc_data['embeddings'][0]
+    query_embedding = doc_data["embeddings"][0]
 
     # Perform the query using the retrieved embedding
     result = collection.query(
@@ -169,37 +178,45 @@ async def query_by_id(query_data: QueryByIDData):
         n_results=query_data.n_results,
         where=query_data.where,
         where_document=query_data.where_document,
-        include=query_data.include
+        include=query_data.include,
     )
     return result
 
-#================================
+
+# ================================
 # Query collection by sentence
 
+
 class QueryBySentenceData(BaseModel):
-    sentence: str = Field(..., description="The sentence to use as the basis for the query.")
+    sentence: str = Field(
+        ..., description="The sentence to use as the basis for the query."
+    )
     n_results: int = Field(4, description="The number of results to return.")
     where: Optional[dict] = Field(None, description="Metadata filter dictionary.")
-    where_document: Optional[dict] = Field(None, description="Document content filter dictionary.")
-    include: Optional[List[str]] = Field(None, description="Fields to include in the response.")
+    where_document: Optional[dict] = Field(
+        None, description="Document content filter dictionary."
+    )
+    include: Optional[List[str]] = Field(
+        None, description="Fields to include in the response."
+    )
+
 
 @app.post("/query_by_sentence")
 async def query_by_sentence(query_data: QueryBySentenceData):
-
     # Perform the query using the computed embedding
     print(query_data)
     result = collection.query(
-        query_texts= query_data.sentence,
+        query_texts=query_data.sentence,
         n_results=query_data.n_results,
         where=query_data.where,
         where_document=query_data.where_document,
-        include=query_data.include
+        include=query_data.include,
     )
     print(result)
     return result
 
 
-#================================
+# ================================
 
 print(get_documents)
 
