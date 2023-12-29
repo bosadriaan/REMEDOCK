1f91bf36765bfdbbb13982389c39be045bb50680
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-12-29 14:07:42 -0300

    al gehad toch

--- chromatest.py
+++ chromatest.py
@@ -1,7 +1,7 @@
 import chromadb
 from chromadb import Settings
 
-chroma_client = chromadb.HttpClient(host='different-insect-production.up.railway.app', port=80)
+chroma_client = chromadb.HttpClient(host='different-insect-production.up.railway.app', port=443, ssl=True)
 print(chroma_client.heartbeat())
 
 from chromadb.utils import embedding_functions
@@ -14,5 +14,11 @@ collection = chroma_client.get_or_create_collection(
     "demo",
     embedding_function=embedding_fn
 )
-collection = chroma_client.create_collection(name="test") 
+collection = chroma_client.get_or_create_collection(name="test") 
 
+collection.add(
+    documents=["lorem ipsum...", "doc2", "doc3"],
+    metadatas=[{"chapter": "3", "verse": "16"}, {"chapter": "3", "verse": "5"}, {"chapter": "29", "verse": "11"}],
+    ids=["id1", "id2", "id3"]
+)
+print(collection.count())
