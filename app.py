d4011b8def3a58cb6a7af73bc3862691decd96ab
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-12-29 13:55:26 -0300

    then 443

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 # client = chromadb.PersistentClient(path="/content/chroma_db")
 # client = chromadb.HttpClient(host="chroma", port=8000) #local Dockerrrr
-client = chromadb.HttpClient(host="different-insect", port=80, ssl=True)
+client = chromadb.HttpClient(host="different-insect", port=443, ssl=True)
 
 # Embedding function setup
 # sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
